c47c701b7f64dac16fbc23acb9f678a6e09da8fd
Roman Samolazov <samolazov.roman.alex@gmail.com>
2020-12-26 08:09:52 +0300

    finish

--- build/js/script.js
+++ build/js/script.js
@@ -1,5 +1,6 @@
 // smooth scrolling
 
+
 const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
@@ -15,7 +16,7 @@ for (let anchor of anchors) {
   })
 }
 
-// slider mot
+slider mot
 
 let btnMot=document.getElementsByClassName('square');
     blockMot=document.getElementsByClassName('mot-content');
@@ -47,3 +48,32 @@ btnMot[2].onclick=function(){
   btnMot[1].style.backgroundColor='#ffffff';
 }
     
+slider work
+
+
+const btnsStructure=document.querySelectorAll('.work__btn');
+const photoBlock=document.querySelector('.work-photo');
+const allPhoto=photoBlock.children;
+const arrows=document.querySelectorAll('.arrow__btn');
+let i=0;
+console.log(allPhoto.length)
+console.log(arrows)
+
+const clickLeft = ()=> i!=0?i--:i=0;
+const clickRight = ()=> i!=allPhoto.length?i++:i=allPhoto.length;
+arrows.forEach(arrow=>{
+  arrow.addEventListener('click',()=>{
+    switch(arrow.id){
+      case 'left': clickLeft();
+        console.log(i)
+        break;
+      case 'right': clickRight();
+        console.log(i);
+        break;
+    }
+    for(let x=0; x<allPhoto.length; x++){
+      x!=i?allPhoto[x].className='work-content-block':allPhoto[x].className='work-content-block block__active_ph';
+    }
+  })
+})
+
